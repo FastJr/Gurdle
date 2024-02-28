e04d9a5d035e42eaa9b8cbe0bdaaecd1f0a41b68
Christian Consiglio <c.consiglio@baypath.net>
2024-02-27 22:00:44 -0500

    Working mit website words url request

--- Gurdle/views.py
+++ Gurdle/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from urllib.request import urlopen
+
+mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
+WORDS = mit_website_text.splitlines()
+for i in range(len(WORDS)):
+    print(WORDS[i].decode('utf-8'))
 
 # Create your views here.
 def gurdle(request):
