df064d729e722175e40f77f0fb8f9519ac4cc33d
Christian Consiglio <c.consiglio@baypath.net>
2024-02-27 22:37:19 -0500

    Printing a random word on refresh to the page successfully

---
+++ Gurdle/utils.py
@@ -0,0 +1,4 @@
+import random
+
+def get_random_word(words_list):
+    return random.choice(words_list)
--- Gurdle/views.py
+++ Gurdle/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from urllib.request import urlopen
+from .utils import get_random_word
 
-mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
-WORDS = mit_website_text.splitlines()
-for i in range(len(WORDS)):
-    print(WORDS[i].decode('utf-8'))
+# mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
+# WORDS = mit_website_text.splitlines()
+# for i in range(len(WORDS)):
+#     print(WORDS[i].decode('utf-8'))
 
 # Create your views here.
 def gurdle(request):
-    return render(request, 'gurdle.html')
+    mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
+    WORDS = mit_website_text.splitlines()
+    decoded_words = [word.decode('utf-8') for word in WORDS]
+    random_word = get_random_word(decoded_words)
+    return render(request, 'gurdle.html', {'WORDS': decoded_words, 'RandomWord': random_word})
