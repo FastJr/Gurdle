e10e9cb9cef755d93da00561cceb8de2579a2c2c
Christian Consiglio <fastjr2002@gmail.com>
2024-03-03 17:07:28 -0500

    Working on html insertion

--- Gurdle/utils.py
+++ Gurdle/utils.py
@@ -2,3 +2,10 @@ import random
 
 def get_random_word(words_list):
     return random.choice(words_list)
+
+def get_random_word_at_length(words_list, length):
+    list_at_length = []
+    for word in words_list:
+        if len(word) == length:
+            list_at_length.append(word)
+    return random.choice(list_at_length)
--- Gurdle/views.py
+++ Gurdle/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from urllib.request import urlopen
-from .utils import get_random_word
+from .utils import get_random_word, get_random_word_at_length
 
 # mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
 # WORDS = mit_website_text.splitlines()
@@ -10,8 +10,11 @@ from .utils import get_random_word
 
 # Create your views here.
 def gurdle(request):
+    functions = {
+        'random'
+    }
     mit_website_text = urlopen("https://www.mit.edu/~ecprice/wordlist.10000").read()
     WORDS = mit_website_text.splitlines()
     decoded_words = [word.decode('utf-8') for word in WORDS]
-    random_word = get_random_word(decoded_words)
+    random_word = get_random_word_at_length(decoded_words, 4)
     return render(request, 'gurdle.html', {'WORDS': decoded_words, 'RandomWord': random_word})
